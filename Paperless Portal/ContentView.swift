b71ec9baeeb6c949c0aac2e289e935e3aace9047
zoeschmitt <zoe4712@gmail.com>
2020-04-29 17:44:11 -0500

    Initial Commit

---
+++ Paperless Portal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Paperless Portal
+//
+//  Created by Zoe Schmitt on 4/29/20.
+//  Copyright © 2020 Zoe Schmitt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
